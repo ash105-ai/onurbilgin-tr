7f2cce07566dbb714591e1fb35f116b1a6715835
Onur Bilgin <onur05bilgin@gmail.com>
2026-09-24 21:01:43 +0300

    logo linkii eklendi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,20 @@
 export default function Navbar() {
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    })
+  }
+
   return (
     <nav className="w-full p-6 flex justify-between items-center bg-slate-900/50 backdrop-blur-md fixed top-0 z-10 border-b border-slate-800">
       
-      <a
-        href="/"
+      <button
+        onClick={goToTop}
         className="text-2xl font-bold text-amber-400 cursor-pointer"
       >
         Onur<span className="text-white">.bilgin</span>
-      </a>
+      </button>
 
       <div className="space-x-8 text-slate-300 hidden md:block">
         <a
@@ -35,4 +42,3 @@ export default function Navbar() {
     </nav>
   )
 }
-```
